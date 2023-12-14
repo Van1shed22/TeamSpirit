6389c193c97975857dea64207ace5875097ce3a1
dudysevam <75519501+dudysevam@users.noreply.github.com>
2023-12-14 19:36:17 +0300

    all

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-u«Z
+let page1_js = document.querySelector('.pageone')
+let page2_js = document.querySelector('.pagetwo')
+let button = document.querySelector('button')
+
+page2_js.style.display='none'
+
+button.addEventListener('click', function(){
+    page2_js.style.display='flex'
+    page1_js.style.display='none'
+    })
