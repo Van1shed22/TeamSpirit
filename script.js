db86911f8e8c92af99e6cc218c0e633cc1ecd9c2
dudysevam <75519501+dudysevam@users.noreply.github.com>
2023-12-21 19:05:36 +0300

    all

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let page1_js = document.querySelector('.pageone')
 let page2_js = document.querySelector('.pagetwo')
-let button = document.querySelector('button')
+let button = document.querySelector('.button1')
+let page3_js = document.querySelector('.pagethree')
+let button1 = document.querySelector('.button2') 
 
 page2_js.style.display='none'
 
 button.addEventListener('click', function(){
     page2_js.style.display='flex'
     page1_js.style.display='none'
+    page3_js.style.display='none'
     })
+    
+page3_js.style.display='none'
+
+button1.addEventListener('click', function(){
+    page3_js.style.display='flex'
+    page2_js.style.display='none'
+    page1_js.style.display='none'
+    })
+    
+    
