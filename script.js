3bf26bdacb6a5df6d090c2d60216e69117a4c946
dudysevam <75519501+dudysevam@users.noreply.github.com>
2023-12-28 17:52:19 +0300

    all

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ let button = document.querySelector('.button1')
 let page3_js = document.querySelector('.pagethree')
 let button1 = document.querySelector('.button2') 
 let button2 = document.querySelector('.button') 
+let button3 = document.querySelector('.button3') 
+let page4_js = document.querySelector('.pagefour')
+
 page2_js.style.display='none'
 
 button.addEventListener('click', function(){
     page2_js.style.display='flex'
     page1_js.style.display='none'
     page3_js.style.display='none'
+    page4_js.style.display='none'
     })
     
 page3_js.style.display='none'
@@ -18,6 +22,7 @@ button1.addEventListener('click', function(){
     page3_js.style.display='flex'
     page2_js.style.display='none'
     page1_js.style.display='none'
+    page4_js.style.display='none'
     })
 
     page2_js.style.display='none'
@@ -26,6 +31,17 @@ button2.addEventListener('click', function(){
     page1_js.style.display='flex'
     page2_js.style.display='none'
     page3_js.style.display='none'
+    page4_js.style.display='none'
     })
+
+    page2_js.style.display='none'
+
+ button3.addEventListener('click', function(){
+        page4_js.style.display='flex'
+        page1_js.style.display='none'
+        page3_js.style.display='none'
+        page2_js.style.display='none'
+        })
+
     
     
